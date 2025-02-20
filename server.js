7feb03dae96ca6b81628be123ae256a219b370e2
benjamin-romm <96504372+benjamin-romm@users.noreply.github.com>
2025-02-20 13:26:06 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+    }
+});
+
+app.use(cors());
+app.use(express.json());
+
+let words = {};
+
+app.post('/submit', (req, res) => {
+    const { word } = req.body;
+    if (word) {
+        words[word] = (words[word] || 0) + 1;
+        io.emit('newWord', word);
+        res.status(200).send({ message: 'Word added successfully' });
+    } else {
+        res.status(400).send({ error: 'Word is required' });
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    socket.emit('initialWords', words);
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is running! Try submitting words.');
+});
